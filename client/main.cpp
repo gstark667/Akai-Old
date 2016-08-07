3c7258581118d9e3110d41ff6354c507bef8b96e
Gavin Stark <gstark31897@gmail.com>
2016-08-06 21:23:30 -0500

    added a config file for server address

--- client/main.cpp
+++ client/main.cpp
@@ -1,3 +1,6 @@
+#include <QtCore/QCoreApplication>
+#include <QtCore/QSettings>
+#include <QtNetwork/QHostAddress>
 #include <QtWidgets/QApplication>
 #include <QtGui/QIcon>
 #include <stdlib.h>
@@ -8,18 +11,25 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 4)
-    {
-        std::cout << argv[0] << " <server-ip> <server-port> <call-port>" << std::endl;
-        return -1;
-    }
+    QCoreApplication::setOrganizationName("SheilDuck");
+    QCoreApplication::setApplicationName("Akai");
+    QSettings settings;
 
-    char *ip = argv[1];
-    int port = atoi(argv[2]);
-    int mport = atoi(argv[3]);
+    if (!settings.contains("server"))
+        settings.setValue("server", "127.0.0.1");
+    if (!settings.contains("port"))
+	settings.setValue("port", 6667);
+    if (!settings.contains("callport"))
+	settings.setValue("callport", 6668);
 
+    qDebug() << settings.fileName();
+
+    QHostAddress serverAddr = QHostAddress(settings.value("server").toString());
+    quint16      serverPort = settings.value("port").toUInt();
+    quint16        callPort = settings.value("callport").toUInt();
+    
     QApplication app (argc, argv);
-    MainWindow *mainWindow = new MainWindow(QHostAddress(ip), port, mport);
+    MainWindow *mainWindow = new MainWindow(serverAddr, serverPort, callPort);
     mainWindow->show();
  
     int return_code = app.exec();
