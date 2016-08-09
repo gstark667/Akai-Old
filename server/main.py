cc5229ccc73ed1617a2037c273d3ad7adf5b250f
Gavin Stark <gstark31897@gmail.com>
2016-08-08 21:41:59 -0500

    adding ssl to server

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from threading import Thread
 from select import select
 from socket import *
+import ssl
 
 from user import User, users, message_queue
 
@@ -10,7 +11,11 @@ if __name__ == '__main__':
     dummy = socket(AF_INET, SOCK_DGRAM)
     dummy.bind(('', 6668))
 
-    server = socket(AF_INET, SOCK_STREAM)
+    server = ssl.wrap_socket(socket(AF_INET, SOCK_STREAM),
+                             server_side=True,
+                             certfile="server.crt",
+                             keyfile="server.key",
+                             ssl_version=ssl.PROTOCOL_TLSv1_2)
     server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     server.bind(('0.0.0.0', 6667))
     server.listen(5)
